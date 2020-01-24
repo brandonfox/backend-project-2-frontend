5cc30de16a8e20a28fb91a9a3cc0b3211e4aa9a1
brandon <brandonfox@hotmail.co.uk>
2020-01-24 23:40:23 +0700

    Moved frontend stuff into its own folder\n Created dockerfile for frontend deployment\n docker-compose now pulls all parts together\n Docker compose still needs nginx router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,22 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 global.axios = axios;
-global.backend = "http://127.0.0.1:25801/api";
+window.console.log(process.env);
+if(process.env.VUE_APP_BACKEND_HOSTNAME !== undefined){
+  global.backend = process.env.VUE_APP_BACKEND_HOSTNAME
+}
+else{
+  global.backend = "http://localhost"
+}
+global.backend += ":";
+if(process.env.VUE_APP_BACKEND_PORT !== undefined){
+  global.backend += process.env.VUE_APP_BACKEND_PORT;
+}else{
+  global.backend += "8080"
+}
+global.backend += "/api";
+
+window.console.log(global.backend);
 
 const router = new VueRouter({
   routes: [
