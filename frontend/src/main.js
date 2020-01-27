3c8bdb24fe0dbd8ebab8b59937f07832d45127b7
brandon <brandonfox@hotmail.co.uk>
2020-01-27 12:03:43 +0700

    Changed dockerfile to properly deploy frontend and configured nginx to follow

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,6 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 global.axios = axios;
-window.console.log(process.env);
 if(process.env.VUE_APP_BACKEND_HOSTNAME !== undefined){
   global.backend = process.env.VUE_APP_BACKEND_HOSTNAME
 }
@@ -23,8 +22,6 @@ if(process.env.VUE_APP_BACKEND_PORT !== undefined){
   global.backend += "8080"
 }
 global.backend += "/api";
-
-window.console.log(global.backend);
 
 const router = new VueRouter({
   routes: [
